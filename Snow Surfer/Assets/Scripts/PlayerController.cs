dc5937d7da5f56646d5aad7de35876c1b0b7c405
aqvoidDev <114750046+aqvoid@users.noreply.github.com>
2025-09-24 18:18:34 +0300

    Added Player Torque Change
    - you can now rotate player with AD/Left or Right arrow keys

--- Snow Surfer/Assets/Scripts/PlayerController.cs
+++ Snow Surfer/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,34 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float torqueAmount = 1f;
+
+    private Rigidbody2D rb;
+
     private InputAction moveAction;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         Vector2 moveVector;
         moveVector = moveAction.ReadValue<Vector2>();
-        Debug.Log(moveVector);
 
+        ChangeTorque(moveVector);
+    }
+
+    private void ChangeTorque(Vector2 moveVector)
+    {
+        if (moveVector.x > 0f) rb.AddTorque(-torqueAmount);
+        else if (moveVector.x < 0f) rb.AddTorque(torqueAmount);
     }
 
 }
