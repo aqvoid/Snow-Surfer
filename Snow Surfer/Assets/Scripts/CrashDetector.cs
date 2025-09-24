4b495d1adfda5885b406c90254f36142ba4db245
aqvoidDev <114750046+aqvoid@users.noreply.github.com>
2025-09-24 19:32:04 +0300

    Finish Line and Crash Detector (WIP)
    - added a delay after winning or losing
    - added a Level 2 scene for victory example

--- Snow Surfer/Assets/Scripts/CrashDetector.cs
+++ Snow Surfer/Assets/Scripts/CrashDetector.cs
@@ -3,11 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] private int restartDelay = 1;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
 
         if (collision.gameObject.layer == layerIndex)
-            SceneManager.LoadScene(0);
+        {
+            Invoke("ReloadScene", restartDelay);
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Snow Surfer/Assets/Scripts/FinishLine.cs
+++ Snow Surfer/Assets/Scripts/FinishLine.cs
@@ -3,11 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private int restartDelay = 1;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
 
         if (collision.gameObject.layer == layerIndex)
-            SceneManager.LoadScene(0);
+        {
+            Invoke("NextScene", restartDelay);
+        }
+    }
+
+    private void NextScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
